bd9239a6f77f11cde9323dbf50d3376021e95b05
Harshpreet Singh Johar <hpsj3112@gmail.com>
2024-01-05 18:09:22 +0530

    committee page added

--- src/pages/committees.js
+++ src/pages/committees.js
@@ -1,12 +1,53 @@
-import PageLayout from '@/components/layout/PageLayout'
-import React from 'react'
+import PageLayout from "@/components/layout/PageLayout";
+import React from "react";
+import { COMMITTEE } from "@/data/committee";
+import styles from "@/styles/pages/committee.module.scss";
 
 function Committees() {
-  return (
-    <PageLayout title={"Committees | Indiscon 2024"}>
+    const keys = Object.keys(COMMITTEE);
+    console.log(keys);
+    return (
+        <PageLayout title={"Committees | Indiscon 2024"}>
+            <h1 className={styles.heading}>Organizing Committee</h1>
 
-    </PageLayout>
-  )
+            <div className={styles.core}>
+                {keys.map((key, i) => {
+                    return (
+                        <>
+                            <h3>{key}</h3>
+                            <div className={styles.cards} key={i}>
+                                {COMMITTEE[key].map((head, i) => {
+                                    const image =
+                                        head.image_url.length != 0
+                                            ? head.image_url
+                                            : "/assets/logos/ieee-logo.png";
+                                    return (
+                                        <div className={styles.card} key={i}>
+                                            {/*  eslint-disable-next-line @next/next/no-img-element */}
+                                            <img
+                                                height={70}
+                                                width={70}
+                                                src={image}
+                                                alt={head.name}
+                                            />
+                                            <div className={styles.data}>
+                                                <p className={styles.name}>
+                                                    {head.name}
+                                                </p>
+                                                <p className={styles.post}>
+                                                    {head.post}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </>
+                    );
+                })}
+            </div>
+        </PageLayout>
+    );
 }
 
-export default Committees
+export default Committees;
