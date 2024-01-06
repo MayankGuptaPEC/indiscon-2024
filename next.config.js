c30dade623a633080a6c822df69710fb276edf51
Harshpreet Singh Johar <hpsj3112@gmail.com>
2024-01-06 22:16:16 +0530

    Update Next.js configuration for static HTML export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   basePath: '/indiscon-2024',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 }
 
 module.exports = nextConfig
