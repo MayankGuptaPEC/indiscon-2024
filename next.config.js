d68521fe412d1f8452eabfeb8b3e89adf0822a96
Harshpreet Singh Johar <hpsj3112@gmail.com>
2024-01-06 22:51:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  basePath: '/indiscon-2024',
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
-}
 
-module.exports = nextConfig
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    basePath: "/indiscon-2024",
+    assetPrefix: "https://mayankguptapec.github.io/indiscon-2024",
+};
+
+module.exports = nextConfig;
