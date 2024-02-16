3b6514251523ee6a6c5dc3795d572922422f36f5
Sudesh Rani <130635615+Jsudesh@users.noreply.github.com>
2024-02-16 17:10:26 +0530

    Update call-for-papers.js
    updated CFP pdf

--- src/pages/call-for-papers.js
+++ src/pages/call-for-papers.js
@@ -23,7 +23,7 @@ function CallForPapers() {
             <div className={styles.paper_wrapper}>
                 <div className={styles.register}>
                     <a
-                        href="/INDISCON2024CFP v1.pdf"
+                        href="/INDISCON2024CFP v2.pdf"
                         target="_blank"
                         referrerPolicy="no-referrer"
                     >
