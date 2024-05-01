b58348d9ea47ba1aafff0c689194332e7bc7ff79
Sudesh Rani <130635615+Jsudesh@users.noreply.github.com>
2024-05-01 13:13:28 +0000

    Update Marquee message

--- src/data/marquee.js
+++ src/data/marquee.js
@@ -1,3 +1,4 @@
 export const ANNOUNCEMENTS = [
-    '🚨 Attention: Deadline Extension! 🚨 The deadline for Regular "Full" Paper Submission, Special Session Proposals, and Tutorial/Workshop Proposals has been extended to April 15, 2024. [Extended Deadline]',
+    // '🚨 Attention: Deadline Extension! 🚨 The deadline for Regular "Full" Paper Submission, Special Session Proposals, and Tutorial/Workshop Proposals has been extended to April 15, 2024. [Extended Deadline]',
+    'Thanks for the overwhelming response for paper submissions in INDISCON 2024. The submission was closed on 15th April 2024. The decisions on submitted papers through given CMT portal will be given in the Month of May 2024.',
 ];
