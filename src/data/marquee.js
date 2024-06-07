5005a210bb33c578d403ce470d429e2bef24baad
Sudesh Rani <130635615+Jsudesh@users.noreply.github.com>
2024-06-07 15:55:29 +0000

    marquee msg

--- src/data/marquee.js
+++ src/data/marquee.js
@@ -7,7 +7,8 @@
 export const ANNOUNCEMENTS = [
     // '🚨 Attention: Deadline Extension! 🚨 The deadline for Regular "Full" Paper Submission, Special Session Proposals, and Tutorial/Workshop Proposals has been extended to April 15, 2024. [Extended Deadline]',
     // 'Thanks for the overwhelming response for paper submissions in INDISCON 2024. The submission was closed on 15th April 2024. The decisions on submitted papers through given CMT portal will be given in the Month of June 2024.',
-    '<a href="/List of Oral and Posters.pdf" target="_blank">Click here to see list of accepted papers</a>',
+    // '<a href="/List of Oral and Posters.pdf" target="_blank">Click here to see list of accepted papers</a>',
+    'Check bottom of this page for List of accepted papers'
 ];
 
 // React component to render the announcements
