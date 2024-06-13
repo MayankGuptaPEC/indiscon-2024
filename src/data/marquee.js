b3ce2ddeef49a14c9a447feb71d9913a9560303b
Sudesh Rani <130635615+Jsudesh@users.noreply.github.com>
2024-06-13 10:51:39 +0000

    camera ready paper

--- src/data/marquee.js
+++ src/data/marquee.js
@@ -8,7 +8,8 @@ export const ANNOUNCEMENTS = [
     // '🚨 Attention: Deadline Extension! 🚨 The deadline for Regular "Full" Paper Submission, Special Session Proposals, and Tutorial/Workshop Proposals has been extended to April 15, 2024. [Extended Deadline]',
     // 'Thanks for the overwhelming response for paper submissions in INDISCON 2024. The submission was closed on 15th April 2024. The decisions on submitted papers through given CMT portal will be given in the Month of June 2024.',
     // '<a href="/List of Oral and Posters.pdf" target="_blank">Click here to see list of accepted papers</a>',
-    'Check bottom of this page for List of accepted papers'
+    // 'Check bottom of this page for List of accepted papers'
+    'The link to submit camera-ready paper will be active on 17 June 2024'
 ];
 
 // React component to render the announcements
