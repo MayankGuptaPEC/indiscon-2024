3531d7ab61331e3e1d106e41f389e2eaf13e4374
Sudesh Rani <130635615+Jsudesh@users.noreply.github.com>
2024-07-25 09:58:56 +0000

    speakers update

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -5,19 +5,19 @@ export const SPEAKERS = [
         designation: "Professor of Nanoelectronics, EEE, University of Manchester, Manchester, UK",
         link: "https://research.manchester.ac.uk/en/persons/a.song",
     },
-    {
-        name: "Prof. Gian Carlo Cardarilli",
-        image: "/assets/speakers/gian.jpeg",
-        designation:
-            'Department of Electronic Engineering, University of Rome “Tor Vergata", Rome, Italy',
-        link: "https://didattica.uniroma2.it/docenti/curriculum/3746-Gian-Carlo-Cardarilli/",
-    },
-    {
-        name: "Prof. Gaurav Sharma",
-        image: "/assets/speakers/gaurav.jpg",
-        designation: "Professor, ECE, University of Rochester, NY, USA",
-        link: "https://hajim.rochester.edu/ece/sites/gsharma/",
-    },
+    // {
+    //     name: "Prof. Gian Carlo Cardarilli",
+    //     image: "/assets/speakers/gian.jpeg",
+    //     designation:
+    //         'Department of Electronic Engineering, University of Rome “Tor Vergata", Rome, Italy',
+    //     link: "https://didattica.uniroma2.it/docenti/curriculum/3746-Gian-Carlo-Cardarilli/",
+    // },
+    // {
+    //     name: "Prof. Gaurav Sharma",
+    //     image: "/assets/speakers/gaurav.jpg",
+    //     designation: "Professor, ECE, University of Rochester, NY, USA",
+    //     link: "https://hajim.rochester.edu/ece/sites/gsharma/",
+    // },
     {
         name: "Prof. Li Qian",
         image: "/assets/speakers/LiQian45.jpg",
@@ -30,16 +30,31 @@ export const SPEAKERS = [
         designation: "Head CART- IIT Delhi, India",
         link: "https://ee.iitd.ac.in/faculty-profile/16",
     },
+    // {
+    //     name: "R. Ahmad Atieh",
+    //     image: "/assets/speakers/ahmad.jpeg",
+    //     designation: "VP, Optiwave Pvt. Ltd., Ottawa, Canada",
+    //     link: "https://www.linkedin.com/in/dr-ahmad-atieh-a9586a62/?originalSubdomain=ca",
+    // },
+    // {
+    //     name: "Mr. Anant Dev Tyagi",
+    //     designation: "Senior Specialist, R&D function, Ericsson LMI (Ireland)",
+    //     image: "/assets/speakers/anant.jpg",
+    //     link: "",
+    // },
     {
-        name: "R. Ahmad Atieh",
-        image: "/assets/speakers/ahmad.jpeg",
-        designation: "VP, Optiwave Pvt. Ltd., Ottawa, Canada",
-        link: "https://www.linkedin.com/in/dr-ahmad-atieh-a9586a62/?originalSubdomain=ca",
+        name: "Prof. B K Panigrahi",
+        image: "/assets/speakers/Dr Chinmoy Saha.jpg",
+        designation: "Professor, Department of Avionics, Indian Institute of Space Science and Technology, Kerala, India",
+        link: "https://www.iist.ac.in/avionics/chinmoysaha",
     },
     {
-        name: "Mr. Anant Dev Tyagi",
-        designation: "Senior Specialist, R&D function, Ericsson LMI (Ireland)",
-        image: "/assets/speakers/anant.jpg",
-        link: "",
+        name: "Prof. B K Panigrahi",
+        image: "/assets/speakers/Dr Chinmoy Saha.jpg",
+        designation: "Professor, Department of Avionics, Indian Institute of Space Science and Technology, Kerala, India",
+        link: "https://www.iist.ac.in/avionics/chinmoysaha",
     },
+
+
+
 ];
