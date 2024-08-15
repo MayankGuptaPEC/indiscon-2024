5b1f0f9837b45bcad7fe4a2568f3b07a1e04b5ae
Mayank Gupta <73732698+MayankGuptaPEC@users.noreply.github.com>
2024-08-15 17:10:19 +0530

    Update speakers.js

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -1,11 +1,36 @@
 export const SPEAKERS = [
-    // {
-    //     name: "Dr. Arvind",
-    //     image: "/assets/committee/Arvind.jpeg",
-    //     designation: "Professor (Physics) & Dean (R&D), Indian Institute of Science Education & Research (IISER) Mohali",
-    //     link: "https://www.iisermohali.ac.in/faculty/dps/arvind",
-    // },
-
+    {
+        name: "Dr. Arvind",
+        image: "/assets/committee/Arvind.jpeg",
+        designation: "Professor (Physics) & Dean (R&D), Indian Institute of Science Education & Research (IISER) Mohali",
+        link: "https://www.iisermohali.ac.in/faculty/dps/arvind",
+    },
+    {
+        name: "Dr. Surendra Pal",
+        image: "/assets/speakers/surendra_pal_photo.jpg",
+        designation: "Deputy Director, Digital & Communications Area, ISRO, Bangalore",
+        link: "https://surendapal1948.tripod.com",
+    },
+    {
+        name: "Prof. Shantanu Bhattacharya",
+        image: "/assets/speakers/Shantanu_Bhattacharya_CSIO.jpeg",
+        designation: "Director, CSIO Chandigarh",
+        link: "https://www.linkedin.com/in/shantanu-bhattacharya-3745831a7/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=in",
+    },
+    
+    {
+        name: "Dr. R.B. Grover",
+        image: "/assets/speakers/R_B_Grover_HBNI.png",
+        designation: "Emeritus Professor, Homi Bhabha National Institute, Mumbai",
+        link: "https://scholar.google.co.in/citations?user=eRsnRJUAAAAJ&hl=en",
+    },
+    
+    {
+        name: "Prof. Sandeep Sancheti",
+        image: "/assets/speakers/Sandeep_Sancheti.jpeg",
+        designation: "VP, Research Relations and Academic Ambassador, Elsevier, India",
+        link: "https://www.linkedin.com/in/prof-dr-sandeep-sancheti-0093b39b/",
+    },
     {
         name: "Prof. Sukumar Mishra",
         image: "/assets/committee/Sukumar Mishra.jpg",
@@ -27,13 +52,55 @@ export const SPEAKERS = [
         designation: "University of Manchester, Manchester, UK",
         link: "https://research.manchester.ac.uk/en/persons/a.song",
     },
-    // {
-    //     name: "Prof. Gian Carlo Cardarilli",
-    //     image: "/assets/speakers/gian.jpeg",
-    //     designation:
-    //         'Department of Electronic Engineering, University of Rome “Tor Vergata", Rome, Italy',
-    //     link: "https://didattica.uniroma2.it/docenti/curriculum/3746-Gian-Carlo-Cardarilli/",
-    // },
+    {
+        name: "Prof. Suvankar Chakraverty",
+        image: "/assets/speakers/Suvankar_Chakraverty_INST_Mohali.jpeg",
+        designation: "Professor and Scientist F, INST Mohali",
+        link: "https://inst.ac.in/scientist/19-suvankar-chakraverty",
+    },
+    {
+        name: "Prof. B K Panigrahi",
+        image: "/assets/committee/bk-panigrahi.jpeg",
+        designation: "Head CART, IIT Delhi",
+        link: "https://ee.iitd.ac.in/faculty-profile/16",
+    },
+    {
+        name: "Prof. Parag Kulkarni",
+        image: "/assets/speakers/Parag Kulkarni.png",
+        designation: "Tokyo International University, Japan",
+        link: "https://www.tiu.ac.jp/etrack/faculty/kulkarni/",
+    },
+    {
+        name: "Prof. Sudeb Dasgupta",
+        image: "/assets/committee/prof-sudeb-dasgupta.jpg",
+        designation: "IIT Roorkee",
+        link: "http://ece.iitr.ac.in/sudeb_dasgupta",
+    },
+    {
+        name: "Dr. Ranjana Sodhi",
+        image: "/assets/speakers/Ranjana_Sodhi_IIT_Ropar.jpg",
+        designation: "Assistant Professor, IIT Ropar",
+        link: "https://iitrpr.ac.in/electrical/rsodhi",
+    },
+    {
+        name: "Dr. Vahid Vahidinasab",
+        image: "/assets/speakers/Vahid_Vahidinasab.png",
+        designation: "Associate Professor, Nottingham Trent University",
+        link: "https://iiitr.ac.in/faculty/hk_sardana",
+    },
+    {
+        name: "Prof. Harish Kumar Sardana",
+        image: "/assets/speakers/H. K. Sardana.jpeg",
+        designation: "Director, IIIT Raichur",
+        link: "https://iiitr.ac.in/faculty/hk_sardana",
+    },
+    {
+        name: "Dr. Gourav Prateek Sharma",
+        image: "/assets/speakers/gpsharma_KTH.jpeg",
+        designation: "PostDoc, KTH Sweden",
+        link: "https://www.kth.se/profile/gpsharma?l=en",
+    },
+
     // {
     //     name: "Prof. Gaurav Sharma",
     //     image: "/assets/speakers/gaurav.jpg",
@@ -46,13 +113,7 @@ export const SPEAKERS = [
         designation: "University of Toronto, Canada",
         link: "https://www.ece.utoronto.ca/people/qian-l/",
     },
-    {
-        name: "Prof. B K Panigrahi",
-        image: "/assets/committee/bk-panigrahi.jpeg",
-        designation: "Head CART, IIT Delhi",
-        link: "https://ee.iitd.ac.in/faculty-profile/16",
-    },
-    // {
+        // {
     //     name: "R. Ahmad Atieh",
     //     image: "/assets/speakers/ahmad.jpeg",
     //     designation: "VP, Optiwave Pvt. Ltd., Ottawa, Canada",
@@ -65,33 +126,78 @@ export const SPEAKERS = [
     //     link: "",
     // },
     {
-        name: "Prof. Chinmoy Saha",
-        image: "/assets/speakers/Dr Chinmoy Saha.jpg",
-        designation: "IIST Thiruvananthapuram",
-        link: "https://www.iist.ac.in/avionics/chinmoysaha",
+        name: "Prof. D. K. Lobiyal",
+        image: "/assets/speakers/D. K. Lobiyal.jpg",
+        designation: "JNU, Delhi",
+        link: "https://www.jnu.ac.in/content/dkl",
     },
-    {
-        name: "Prof. N. P. Padhy",
-        image: "/assets/committee/NP Padhy.jpg",
-        designation: "Director, MNIT Jaipur",
-        link: "https://mnit.ac.in/admin/director",
-    },
-
-
     {
         name: "Dr. Surender Reddy Salkuti",
         image: "/assets/speakers/Salkuti.jpg",
         designation: "Woosong University, South Korea",
         link: "https://scholar.google.co.in/citations?user=yZze_g4AAAAJ&hl=en",
     },
-
     {
-        name: "Mr. R. K. Tyagi",
-        image: "/assets/speakers/RKTyagi.png",
-        designation: "CMD, PGCIL",
-        link: "https://www.powergrid.in/en/cmd-powergrid",
+        name: "Dr. Gurjeet Singh Walia",
+        image: "/assets/speakers/GSWalia.gif",
+        designation: "DRDO, Delhi",
+        link: "https://ieeexplore.ieee.org/author/38667438100",
     },
-
+    {
+        name: "Prof. Debabrata Das",
+        image: "/assets/committee/prof-debbrata-das.jpg",
+        designation: "Director, IIIT Bangalore",
+        link: "https://www.iiitb.ac.in/faculty/debabrata-das/",
+    },
+    {
+        name: "Prof. Bhavesh Kumar Bhalja",
+        image: "/assets/speakers/Bhavesh_Kumar_Bhalja.jpg",
+        designation: "Professor, IIT Roorkee",
+        link: "https://www.iitr.ac.in/~EE/Bhavesh_Bhalja",
+    },
+    {
+        name: "Dr. Nitin Auluck",
+        image: "/assets/speakers/Nitin_Auluck_IIT_Ropar.png",
+        designation: "Associate Professor, IIT Ropar",
+        link: "https://iitrpr.irins.org/profile/62408",
+    },
+    {
+        name: "Dr. G. Shrikanth Reddy",
+        image: "/assets/speakers/GSReddy_IIT_Mandi.jpg",
+        designation: "Associate Professor, IIT Mandi",
+        link: "https://faculty.iitmandi.ac.in/~gopishrikanth/",
+    },
+    
+    {
+        name: "Dr. Ritula Thakur",
+        image: "/assets/speakers/Ritula_Thakur_NITTTR_Chandigarh.jpg",
+        designation: "Associate Professor, NITTTR Chandigarh",
+        link: "https://vidwan.inflibnet.ac.in/profile/170739",
+    },
+    {
+        name: "Prof. Prerna Gaur",
+        image: "/assets/committee/prof-prerna-gaur.jpeg",
+        designation: "Professor, NSUT New Delhi",
+        link: "http://nsut.ac.in/en/node/547",
+    },
+    {
+        name: "Dr. Saptarshi Das",
+        image: "/assets/speakers/Saptarshi Das.jpg",
+        designation: "Pennsylvania State University, USA",
+        link: "https://www.matse.psu.edu/directory/saptarshi-das",
+    },
+    {
+        name: "Prof. Gian Carlo Cardarilli",
+        image: "/assets/speakers/gian.jpeg",
+        designation: 'Department of Electronic Engineering, University of Rome “Tor Vergata", Rome, Italy',
+        link: "https://didattica.uniroma2.it/docenti/curriculum/3746-Gian-Carlo-Cardarilli/",
+    },
+    {
+        name: "Prof. Ashwani Kumar",
+        image: "/assets/speakers/Ashwani_Kumar_NIT_Kurukshetra.jpg",
+        designation: "Professor, NIT Kurukshetra",
+        link: "https://nitkkr.ac.in/author/ashavani-kumar/",
+    },
     {
         name: "Prof. C. C. Reddy",
         image: "/assets/speakers/Dr. C C Reddy.jpg",
@@ -99,127 +205,137 @@ export const SPEAKERS = [
         link: "https://www.iitrpr.ac.in/electrical/reddy",
     },
     {
-        name: "Prof. G. Bhuvneswari",
-        image: "/assets/committee/prof-G-Bhuvaneswari.jpg",
-        designation: "Mahindra University, Hyderabad",
-        link: "https://www.mahindrauniversity.edu.in/faculty/bhuvaneswari-gurumoorthy/",
+        name: "Dr. N Banumathy",
+        image: "/assets/speakers/N_Banumathy_PGIMER.jpg",
+        designation: "Assistant Professor, PGIMER Chandigarh",
+        link: "https://scholar.google.com/citations?user=kw28BxwAAAAJ&hl=en",
     },
-
-    {
-        name: "Prof. Jaleel Akhtar",
-        image: "/assets/speakers/jaleel akhtar.png",
-        designation: "IIT Kanpur",
-        link: "https://home.iitk.ac.in/~mjakhtar",
-    },
-{
-        name: "Prof. Shiban Koul",
-        image: "/assets/speakers/shiban koul.jpg",
-        designation: "IIT Delhi",
-        link: "https://iitd.irins.org/profile/60674",
-    },
-{
-        name: "Dr. Ramesh Gupta",
-        image: "/assets/speakers/Ramesh gupta.jpg",
-        designation: "VP, Ligado Networks, USA",
-        link: "https://www.linkedin.com/in/rameshgupta",
-    },
-
     {
         name: "Dr. Shahrir Kasjoo",
         image: "/assets/speakers/Shahrir Kasjoo.jpg",
         designation: "Universiti Malaysia Perlis, Malaysia",
         link: "https://scholar.google.com.my/citations?user=8FXQY2kAAAAJ&hl=en",
     },
-{
-        name: "Prof. V. Ramgopal Rao",
-        image: "/assets/speakers/prof-vrao.jpg",
-        designation: "VC, BITS Pilani",
-        link: "https://www.bits-pilani.ac.in/pilani/v-ramgopal-rao",
+    {
+        name: "Dr. Satish Kumar",
+        image: "/assets/speakers/Satish_Kumar_CSIO.jpeg",
+        designation: "Chief Scientist, CSIO Chandigarh",
+        link: "https://icsio.csio.res.in/CSIO.EMS/HoD.aspx?id=746",
     },
-{
-        name: "Prof. Rudra Pratap",
-        image: "/assets/speakers/Dr Rudra Pratap.png",
-        designation: "Founding VC, Plaksha University, SAS Nagar",
-        link: "https://plaksha.edu.in/faculty-details/dr-rudra-pratap/",
+    {
+        name: "Dr. Arun K. Verma",
+        image: "/assets/committee/Dr. Arun Kumar Verma.jpg",
+        designation: "Associate Professor, IIT Jammu",
+        link: "https://www.iitjammu.ac.in/faculty/~arunkumarverma",
     },
-{
-        name: "Prof. Sudeb Dasgupta",
-        image: "/assets/committee/prof-sudeb-dasgupta.jpg",
-        designation: "IIT Roorkee",
-        link: "http://ece.iitr.ac.in/sudeb_dasgupta",
-    },
-{
-        name: "Prof. D. K. Lobiyal",
-        image: "/assets/speakers/D. K. Lobiyal.jpg",
-        designation: "JNU, Delhi",
-        link: "https://www.jnu.ac.in/content/dkl",
-    },
-{
+    {
         name: "Prof. Bob Gill",
         image: "/assets/speakers/Bob Gill.jpeg",
         designation: "BCIT, Canada",
         link: "https://www.linkedin.com/in/bob-gill-p-190a1117/?originalSubdomain=ca",
     },
-{
-        name: "Prof. Debabrata Das",
-        image: "/assets/committee/prof-debbrata-das.jpg",
-        designation: "Director, IIIT Bangalore",
-        link: "https://www.iiitb.ac.in/faculty/debabrata-das/",
+    {
+        name: "Dr. Seshan Srirangarajan",
+        image: "/assets/speakers/Seshan_Srirangarajan_IIT_Delhi.jpeg",
+        designation: "Associate Professor, IIT Delhi",
+        link: "https://web.iitd.ac.in/~seshan/",
     },
-{
-        name: "Prof. Parag Kulkarni",
-        image: "/assets/speakers/Parag Kulkarni.png",
-        designation: "Tokyo International University, Japan",
-        link: "https://www.tiu.ac.jp/etrack/faculty/kulkarni/",
+    {
+        name: "Dr. Anirban Sarkar",
+        image: "/assets/speakers/Anirban Sarkar IIT Mandi.jpeg",
+        designation: "Assistant Professor, IIT Mandi",
+        link: "https://scee.iitmandi.ac.in/faculty_personal.php?id=36",
     },
-{
-        name: "Dr. Saptarshi Das",
-        image: "/assets/speakers/Saptarshi Das.jpg",
-        designation: "Pennsylvania State University, USA",
-        link: "https://www.matse.psu.edu/directory/saptarshi-das",
-    },
-{
+    {
         name: "Prof. Jawar Singh",
         image: "/assets/committee/prof-jawar-singh.jpg",
         designation: "IIT Patna",
         link: "https://www.iitp.ac.in/?view=article&id=191&catid=2",
     },
-{
-        name: "Prof. Harish Kumar Sardana",
-        image: "/assets/speakers/H. K. Sardana.jpeg",
-        designation: "Director, IIIT Raichur",
-        link: "https://iiitr.ac.in/faculty/hk_sardana",
-    },
-{
-        name: "Mr. Mukul Gupta",
-        image: "/assets/speakers/mukul gupta.jpeg",
-        designation: "Dynapt Technologies, Delhi",
-        link: "https://www.linkedin.com/in/mukulgupta1/?originalSubdomain=in",
-    },
-{
-        name: "Dr. Anil Yadav",
-        image: "/assets/speakers/anil yadav.jpeg",
-        designation: "Samsung R&D Institute, Noida",
-        link: "https://www.linkedin.com/in/anyadav/?originalSubdomain=in",
-    },
-{
-        name: "Prof. Shahar Kvatinsky",
-        image: "/assets/speakers/pic-shahar-kvatinsky.jpg",
-        designation: "Israel Institute of Technology, Israel",
-        link: "https://kvatinsky.com/",
-    },
-{
-        name: "Prof. Narendra S. Chaudhari",
-        image: "/assets/committee/prof-narendra-chaudhari.jpg",
-        designation: "IIT, Indore",
-        link: "https://www.iiti.ac.in/people/~narendra",
-    },
-{
-        name: "Dr. Gurjit Singh Walia",
-        image: "/assets/speakers/GSWalia.gif",
-        designation: "DRDO, Delhi",
-        link: "https://ieeexplore.ieee.org/author/38667438100",
-    },
+
+//     {
+//         name: "Prof. Chinmoy Saha",
+//         image: "/assets/speakers/Dr Chinmoy Saha.jpg",
+//         designation: "IIST Thiruvananthapuram",
+//         link: "https://www.iist.ac.in/avionics/chinmoysaha",
+//     },
+//     {
+//         name: "Prof. N. P. Padhy",
+//         image: "/assets/committee/NP Padhy.jpg",
+//         designation: "Director, MNIT Jaipur",
+//         link: "https://mnit.ac.in/admin/director",
+//     },
+
+//     {
+//         name: "Mr. R. K. Tyagi",
+//         image: "/assets/speakers/RKTyagi.png",
+//         designation: "CMD, PGCIL",
+//         link: "https://www.powergrid.in/en/cmd-powergrid",
+//     },
+
+//     {
+//         name: "Prof. G. Bhuvneswari",
+//         image: "/assets/committee/prof-G-Bhuvaneswari.jpg",
+//         designation: "Mahindra University, Hyderabad",
+//         link: "https://www.mahindrauniversity.edu.in/faculty/bhuvaneswari-gurumoorthy/",
+//     },
+
+//     {
+//         name: "Prof. Jaleel Akhtar",
+//         image: "/assets/speakers/jaleel akhtar.png",
+//         designation: "IIT Kanpur",
+//         link: "https://home.iitk.ac.in/~mjakhtar",
+//     },
+// {
+//         name: "Prof. Shiban Koul",
+//         image: "/assets/speakers/shiban koul.jpg",
+//         designation: "IIT Delhi",
+//         link: "https://iitd.irins.org/profile/60674",
+//     },
+// {
+//         name: "Dr. Ramesh Gupta",
+//         image: "/assets/speakers/Ramesh gupta.jpg",
+//         designation: "VP, Ligado Networks, USA",
+//         link: "https://www.linkedin.com/in/rameshgupta",
+//     },
+
+// {
+//         name: "Prof. V. Ramgopal Rao",
+//         image: "/assets/speakers/prof-vrao.jpg",
+//         designation: "VC, BITS Pilani",
+//         link: "https://www.bits-pilani.ac.in/pilani/v-ramgopal-rao",
+//     },
+// {
+//         name: "Prof. Rudra Pratap",
+//         image: "/assets/speakers/Dr Rudra Pratap.png",
+//         designation: "Founding VC, Plaksha University, SAS Nagar",
+//         link: "https://plaksha.edu.in/faculty-details/dr-rudra-pratap/",
+//     },
+
+// {
+//         name: "Mr. Mukul Gupta",
+//         image: "/assets/speakers/mukul gupta.jpeg",
+//         designation: "Dynapt Technologies, Delhi",
+//         link: "https://www.linkedin.com/in/mukulgupta1/?originalSubdomain=in",
+//     },
+// {
+//         name: "Dr. Anil Yadav",
+//         image: "/assets/speakers/anil yadav.jpeg",
+//         designation: "Samsung R&D Institute, Noida",
+//         link: "https://www.linkedin.com/in/anyadav/?originalSubdomain=in",
+//     },
+// {
+//         name: "Prof. Shahar Kvatinsky",
+//         image: "/assets/speakers/pic-shahar-kvatinsky.jpg",
+//         designation: "Israel Institute of Technology, Israel",
+//         link: "https://kvatinsky.com/",
+//     },
+// {
+//         name: "Prof. Narendra S. Chaudhari",
+//         image: "/assets/committee/prof-narendra-chaudhari.jpg",
+//         designation: "IIT, Indore",
+//         link: "https://www.iiti.ac.in/people/~narendra",
+//     },
 
 
     
